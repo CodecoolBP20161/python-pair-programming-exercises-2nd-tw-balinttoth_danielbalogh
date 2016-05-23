78fd6e4e3cac7a6292e77eb7fe731a399f20ad7a
TBalint19 <toth910719balint@gmail.com>
2016-05-23 12:04:08 +0200

    First commit

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,8 +1,18 @@
 def fizzbuzz(number):
+    if (float(number) / 15) == round(number / 15) :
+        print("Fizzbuzz")
+    elif (float(number) / 3) == round(number / 3) :
+        print("Fizz ")
+    elif (float(number) / 5) == round(number / 5) :
+        print("Buzz")
+    else :
+        print number
     return
 
 
 def main():
+    for x in range(1, 101):
+        fizzbuzz(x)
     return
 
 if __name__ == '__main__':
