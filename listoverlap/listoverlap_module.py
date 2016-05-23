a999344cd576abf6b2c575daba231d420b80a74f
TBalint19 <toth910719balint@gmail.com>
2016-05-23 13:53:05 +0200

    h

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -1,9 +1,20 @@
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
 def listoverlap(list1, list2):
+    set1 = set(list1)
+    set2 = set(list2)
+
+    sets = (set1 & set2)
+    lists = list(sets)
+    print (lists)
+
     return
 
 
 def main():
-    return
+    listoverlap(a, b)
+    return 
 
 
 if __name__ == '__main__':
