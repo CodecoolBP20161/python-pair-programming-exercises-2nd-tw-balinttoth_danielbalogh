40172cd2b84bd6ffad05f573a2ff73d8e1c5e83d
TBalint19 <toth910719balint@gmail.com>
2016-05-25 09:41:31 +0200

    years done

--- years/years_module.py
+++ years/years_module.py
@@ -2,10 +2,17 @@ import datetime
 
 
 def years(age):
-    return
+    x = datetime.datetime.now()
+    this_year = x.year
+    one_hunder_years_old = int(this_year) - int(age) + 100
+    return one_hunder_years_old
 
 
 def main():
+    name = input("What is your name?: ")
+    age = (input("How old are you?: "))
+    years(age)
+    print("Dear %s, I would like to inform you, that you will turn 100 in %s." % (name, years(age)))
     return
 
 
